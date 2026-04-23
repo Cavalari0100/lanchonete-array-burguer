a4b0e9b67f9d123f4b5d0d829ea143c0d094c580
Cavalari0100 <joao.cavalarispa@hotmail.com>
2026-04-23 11:31:52 -0300

    adcionando estilos css

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Array Burguer</title>
 </head>
+
 <body>
-    
+    <nav>
+        <ul>
+            <li><img src="assets/images/logo.png" alt="Logo Array Burguer"></li>
+            <li><a href="cadastro.php">Cadastro de Produtos</a></li>
+            <li><a href="listar.php">Listar Produtos</a></li>
+        </ul>
+    </nav>
 </body>
+
 </html>
