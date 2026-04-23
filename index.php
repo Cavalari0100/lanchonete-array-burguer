7b9727e1e55d74312d2b3a800d08699e9c0279c7
Cavalari0100 <joao.cavalarispa@hotmail.com>
2026-04-23 13:49:04 -0300

    ajustes no menu de lanches disponiveis

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+$lanches = [
+    [
+        'nome' => 'X-Burguer',
+        'descricao' => 'Hambúrguer clássico com queijo, alface, tomate e maionese.',
+        'preco' => 15.00,
+        'imagem' => 'https://api-middleware-mcd.mcdonaldscupones.com/media/image/product$300x225_Frango.png/200/200/original?country=br'
+    ],
+    [
+        'nome' => 'X-Salada',
+        'descricao' => 'Hambúrguer com queijo, alface, tomate, cebola e maionese.',
+        'preco' => 18.00,
+        'imagem' => 'https://api-middleware-mcd.mcdonaldscupones.com/media/image/product$300x225_Carne.png/200/200/original?country=br'
+    ],
+    [
+        'nome' => 'X-Bacon',
+        'descricao' => 'Hambúrguer com queijo, bacon crocante, alface, tomate e maionese.',
+        'preco' => 20.00,
+        'imagem' => 'https://api-middleware-mcd.mcdonaldscupones.com/media/image/product$k6X0kr6l/200/200/original?country=br'
+    ]
+];
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -24,42 +47,23 @@
         <p>O melhor lugar para encontrar os melhores hambúrgueres da cidade.</p>
         <img class="banner-img" src="assets/images/banner.png" alt="Imagem de um hambúrguer suculento">
     </main>
-
     <section class="sessao-lanches">
         <h2>Confira nossos lanches</h2>
+        <hr>
         <div class="lanche-container">
             <?php
             // Array de lanches
-            $lanches = [
-                [
-                    'nome' => 'X-Burguer',
-                    'descricao' => 'Hambúrguer clássico com queijo, alface, tomate e maionese.',
-                    'preco' => 15.00,
-                    'imagem' => 'https://api-middleware-mcd.mcdonaldscupones.com/media/image/product$300x225_Frango.png/200/200/original?country=br'
-                ],
-                [
-                    'nome' => 'X-Salada',
-                    'descricao' => 'Hambúrguer com queijo, alface, tomate, cebola e maionese.',
-                    'preco' => 18.00,
-                    'imagem' => 'https://api-middleware-mcd.mcdonaldscupones.com/media/image/product$300x225_Carne.png/200/200/original?country=br'
-                ],
-                [
-                    'nome' => 'X-Bacon',
-                    'descricao' => 'Hambúrguer com queijo, bacon crocante, alface, tomate e maionese.',
-                    'preco' => 20.00,
-                    'imagem' => 'https://api-middleware-mcd.mcdonaldscupones.com/media/image/product$k6X0kr6l/200/200/original?country=br'
-                ]
-            ];
-            echo '<div class="lanche">';
             foreach ($lanches as $lanche) {
+                echo '<div class="lanche">';
                 echo '<img src="' . $lanche['imagem'] . '" alt="' . $lanche['nome'] . '">';
                 echo '<h3>' . $lanche['nome'] . '</h3>';
                 echo '<p>' . $lanche['descricao'] . '</p>';
                 echo '<p>Preço: R$ ' . number_format($lanche['preco'], 2, ',', '.') . '</p>';
+                echo '<button class="btn-comprar">Comprar</button>';
                 echo '</div>';
             }
-
             ?>
+
         </div>
     </section>
 
